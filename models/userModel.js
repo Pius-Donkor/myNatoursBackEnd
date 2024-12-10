96edb2d260f688f37b684e775e82664853e2e86b
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-12-10 13:15:34 +0000

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -29,7 +29,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minLength: 8
+    minLength: 8,
+    select: false
   },
   passwordConfirm: {
     type: String,
@@ -43,6 +44,13 @@ const userSchema = new mongoose.Schema({
     }
   }
 });
+
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
