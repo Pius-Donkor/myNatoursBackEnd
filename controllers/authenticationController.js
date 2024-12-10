3ba3e9b7b0226c0ab0fc3a7ae4880cec56959a42
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-12-10 13:15:29 +0000

    Update authenticationController.js

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,10 +1,43 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const signupToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+  const token = signupToken(newUser._id);
   res.status(201).json({
     status: 'success',
+    token,
     user: newUser
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return next(new AppError('Provide an email and password', 400));
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!email || !(await user.correctPassword(password, user.password)))
+    return next(new AppError('Incorrect email or password', 401));
+
+  const token = signupToken();
+  res.status(201).json({
+    status: 'success',
+    token
   });
 });
