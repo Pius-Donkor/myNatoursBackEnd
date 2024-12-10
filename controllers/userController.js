b83469b4593e17116141e79866d13e8470bb93b9
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-12-10 13:15:31 +0000

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,14 @@
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not implemented'
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: 'success',
+    result: users.length,
+    data: { users }
   });
-};
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not implemented'
-  });
-};
+});
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
