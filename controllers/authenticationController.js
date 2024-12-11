cee15acd43a528d18c9ee938d82358662d79ec7e
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-12-11 15:02:47 +0000

    Update authenticationController.js

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -15,7 +16,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm
+    passwordConfirm: req.body.passwordConfirm,
+    changedPasswordAt: req.body.changedPasswordAt
   });
   const token = signupToken(newUser._id);
   res.status(201).json({
@@ -35,9 +37,50 @@ exports.login = catchAsync(async (req, res, next) => {
   if (!email || !(await user.correctPassword(password, user.password)))
     return next(new AppError('Incorrect email or password', 401));
 
-  const token = signupToken();
+  const token = signupToken(user._id);
   res.status(201).json({
     status: 'success',
     token
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  // console.log(token);
+
+  if (!token) {
+    return next(
+      new AppError('You are not yet logged in , Please login to get access')
+    );
+  }
+  // 2)verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does not exist anymore',
+        401
+      )
+    );
+  }
+  // 4) check if user changed password after the token was issued
+
+  if (currentUser.changedPassword(decoded.iat)) {
+    return next(
+      new AppError('user currently changed password, Please login again', 401)
+    );
+  }
+  req.user = currentUser;
+  // moving to the next middleware
+  next();
 });
