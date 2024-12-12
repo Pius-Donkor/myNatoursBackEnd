87eef2b6f22a45dfae187cd1700fe080d84a4d3f
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-12-12 23:39:45 +0000

    Update authenticationController.js

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -4,6 +4,7 @@ const { promisify } = require('util');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const sendEmail = require('../utils/email');
 
 const signupToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -97,3 +98,42 @@ exports.restrictTo = function(...roles) {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with this email !', 404));
+  }
+  // 2) generate random token
+  const resetToken = user.createPasswordResetToken();
+  user.save({ validateBeforeSave: false });
+
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password ? Submit a path request with your new password and passwordConfirm to :${resetUrl}.\n if you didn't forget your password, please ignore this email!`;
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 min)',
+      message
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'token sent to email'
+    });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError(
+        'There was an error sending the email, PLease try again later!',
+        500
+      )
+    );
+  }
+});
