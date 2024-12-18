21178c7321fbd32d128b961526d5d35be088b4e3
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-12-18 16:04:50 +0000

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,12 @@ const authenticationController = require('./../controllers/authenticationControl
 router.post('/signup', authenticationController.signup);
 router.post('/login', authenticationController.login);
 router.post('/forgotPassword', authenticationController.forgotPassword);
-router.post('/resetPassword/:token', authenticationController.resetPassword);
+router.patch('/resetPassword/:token', authenticationController.resetPassword);
+router.patch(
+  '/changeMyPassword',
+  authenticationController.protect,
+  authenticationController.updatePassword
+);
 
 router.route('/').get(userController.getAllUsers);
 router
