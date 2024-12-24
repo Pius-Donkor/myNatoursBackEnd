7e60caca9f5fee57ded564e1461485ff3192585a
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-12-24 05:18:55 +0000

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllUsers = catchAsync(async (req, res) => {
@@ -9,6 +10,49 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     data: { users }
   });
 });
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) check if user is not trying to update password
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'this route is not for password updates, please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+  // 2) filter the body object
+  const filteredObj = filterObj(req.body, 'email', 'name');
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredObj, {
+    new: true,
+    runValidators: true
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedUser
+    }
+  });
+});
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  console.log(req.user);
+  await User.findByIdAndUpdate(req.user._id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
