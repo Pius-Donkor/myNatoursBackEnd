ac63f3c661e58eb939ef6ca1c839d339a5de6981
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-12-24 05:19:21 +0000

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,6 +13,16 @@ router.patch(
   authenticationController.protect,
   authenticationController.updatePassword
 );
+router.patch(
+  '/updateMe',
+  authenticationController.protect,
+  userController.updateMe
+);
+router.delete(
+  '/deleteMe',
+  authenticationController.protect,
+  userController.deleteMe
+);
 
 router.route('/').get(userController.getAllUsers);
 router
