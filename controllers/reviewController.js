654a8b99d6838e0f705f18a452a14e5c3697b002
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2024-12-26 23:35:28 +0000

    Create reviewController.js

---
+++ controllers/reviewController.js
@@ -0,0 +1,41 @@
+const Review = require('../models/reviewModel');
+const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      newReview
+    }
+  });
+});
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Review.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const reviews = await features.query;
+  res.status(200).json({
+    status: 'success',
+    data: {
+      reviews
+    }
+  });
+});
+
+exports.getReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+  if (!review) {
+    return next(new AppError('There is no review with this ID', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { review }
+  });
+});
