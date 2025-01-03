998fcdf093d869482bf7ff009e1884f44501bc7e
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2025-01-03 23:08:17 +0000

    Update reviewController.js

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,30 +1,23 @@
 const Review = require('../models/reviewModel');
-const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handleFactory');
 
-exports.createReview = catchAsync(async (req, res, next) => {
-  const newReview = await Review.create(req.body);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      newReview
-    }
-  });
+exports.setUserTourIds = catchAsync(async (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user._id;
+  next();
 });
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(Review.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-
-  const reviews = await features.query;
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const newReviews = await Review.find(filter);
   res.status(200).json({
     status: 'success',
+    result: newReviews.length,
     data: {
-      reviews
+      newReviews
     }
   });
 });
@@ -39,3 +32,7 @@ exports.getReview = catchAsync(async (req, res, next) => {
     data: { review }
   });
 });
+
+exports.createReview = factory.createOne(Review);
+exports.deleteReview = factory.deleteOne(Review);
+exports.updataReview = factory.updateOne(Review);
