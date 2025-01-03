3a08d852f0d7f03dba45004b5779ea37c9fbe33f
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2025-01-03 23:07:48 +0000

    Update tourRoutes.js

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authenticationController = require('./../controllers/authenticationController');
+const reviewRouter = require('./reviewsRoute');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
 // alias routing
+
+// NESTED ROUTES
+// POST   /tour/242354/reviews
+// GET   /tour/242354/reviews
+// POST /tour/242354/reviews/552365
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authenticationController.protect,
+//     authenticationController.restrictTo('user'),
+//     reviewController.createReview
+//   );
+
+// here we were implementing nested by using the ( reviewController.createReview) which was later  updated by relocating any route string that looked like "/:tourId/reviews"  to the reviewRouter.
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
