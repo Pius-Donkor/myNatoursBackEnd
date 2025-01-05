a06fd88715ed60e347225214522fa4d09f1d532a
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2025-01-05 23:00:55 +0000

    Update tourController.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,6 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
-const AppError = require('./../utils/appError');
+// const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handleFactory');
 
@@ -11,38 +11,9 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = catchAsync(async (req, res, next) => {
-  // console.log(req.query);
+exports.getAllTours = factory.getAll(Tour);
 
-  // EXECUTE QUERY
-
-  const features = new APIFeatures(Tour.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-
-  const tours = await features.query;
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    length: tours.length,
-    data: { tours }
-  });
-});
-exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id).populate('reviews');
-  // sometimes when the id string is valid but not correct, so in order to properly handle such an error this is what we need to do.
-  if (!tour) {
-    return next(new AppError('could not find tour with this ID', 404));
-  }
-  res.status(200).json({
-    status: 'success',
-    data: { tour }
-  });
-});
-
+exports.getTour = factory.getOne(Tour, { path: 'reviews' });
 exports.createTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
 exports.deleteTour = factory.deleteOne(Tour);
