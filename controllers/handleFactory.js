0d93214757f69c6d9ac2b84fa40e9a0627c40d5f
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2025-01-05 23:00:42 +0000

    Update handleFactory.js

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -1,3 +1,4 @@
+const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
@@ -38,5 +39,43 @@ exports.createOne = Model =>
       data: {
         data: doc
       }
+    });
+  });
+
+exports.getOne = (Model, populateOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (populateOptions) query = query.populate(populateOptions);
+    const doc = await query;
+    // sometimes when the id string is valid but not correct, so in order to properly handle such an error this is what we need to do.
+    if (!doc) {
+      return next(new AppError('could not find document with this ID', 404));
+    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
+
+exports.getAll = Model =>
+  catchAsync(async (req, res, next) => {
+    // console.log(req.query);
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    // EXECUTE QUERY
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const doc = await features.query;
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      length: doc.length,
+      data: { data: doc }
     });
   });
