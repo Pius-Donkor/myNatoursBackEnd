0ca2c8559e5acd3d792d0202b95d89b2f4ab2bbf
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2025-01-11 21:54:33 +0000

    section one of server side rendering with pug

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -3,10 +3,16 @@ const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res) => {
   const tours = await Tour.find();
-
   res.status(200).render('overview', { tours, title: 'All Tour' });
 });
 
-exports.getTourDetail = (req, res) => {
-  res.status(200).render('tour', { title: 'The Forest Hiker' });
-};
+exports.getTour = catchAsync(async (req, res) => {
+  const { slug } = req.params;
+  // console.log(slug);
+  const tour = await Tour.findOne({ slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
+  });
+  // console.log(tour);
+  res.status(200).render('tour', { title: 'The forest hiker', tour });
+});
